0d2f7e95724647e729cab92152c2062e131c7cba
yt2014 <taoy_2007@126.com>
2015-06-04 23:43:24 +0800

    ui design

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include <QApplication>
+#include <QSqlDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC","Dail");
+    db.setDatabaseName("DRIVER={Microsoft Access Driver (*.mdb)};FIL={MS Access};DBQ=dail.mdb");
+
     MainWindow w;
     w.show();
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,7 @@
 
 #include <QToolButton>
 #include <QTextCodec>
+#include <QSqlDatabase>
 
 #pragma warning(disable: 4819)
 
@@ -29,6 +30,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     this->setWindowFlags(Qt::FramelessWindowHint);
     this->setWindowOpacity(1);
+ //   this->setWindowFlags(Qt::WindowMinimizeButtonHint);
 
     QTextCodec *codec = QTextCodec::codecForName("GBK");
     QTextCodec::setCodecForLocale(codec);
@@ -39,18 +41,86 @@ MainWindow::MainWindow(QWidget *parent) :
     QToolButton *tBtn_Contractors = new QToolButton;
     tBtn_Contractors->setText(QStringLiteral("联系人"));
     QToolButton *tBtn_Dail = new QToolButton;
-    tBtn_Dail->setText(QStringLiteral("拨  号"));
+    tBtn_Dail->setText(QStringLiteral("通话记录"));
 
     ui->mainToolBar->addWidget(tBtn_Contractors);
     ui->mainToolBar->addWidget(tBtn_Dail);
 
     ui->mainToolBar->hide();
-    this->ui->pBtn_Contactors->setFlat(1);
-    this->ui->pBtn_Dail->setFlat(1);
+    //this->ui->pBtn_Contactors->setFlat(1);
+    //this->ui->pBtn_Dail->setFlat(1);
+    //this->ui->pBtn_Dail->setStyleSheet("background-color:transparent;");
+    font.setPointSize(10);
+
+    ui->tabWidget->setFont(font);
+
+    QSqlDatabase db = QSqlDatabase::database("Dail",true);
+
+    if(db.isValid()&&db.isOpen())
+    {
+        ui->label_indications->setText("database opened successfully!");
+     //   ui->label_indications->setText("");
+    }
+    else
+    {
+        ui->label_indications->setText("can't open database!");
+    }
+
+    int width = this->width();//获取界面的宽度
+
+     //构建最小化、最大化、关闭按钮
+     QToolButton *minButton = new QToolButton(this);
+     QToolButton *closeButton= new QToolButton(this);
+
+     //获取最小化、关闭按钮图标
+     QPixmap minPix  = style()->standardPixmap(QStyle::SP_TitleBarMinButton);
+     QPixmap closePix = style()->standardPixmap(QStyle::SP_TitleBarCloseButton);
+
+     //设置最小化、关闭按钮图标
+     minButton->setIcon(minPix);
+     closeButton->setIcon(closePix);
+     //设置最小化、关闭按钮在界面的位置
+     minButton->setGeometry(width-46,5,20,20);
+     closeButton->setGeometry(width-25,5,20,20);
+
+     //设置鼠标移至按钮上的提示信息
+     minButton->setToolTip(tr("最小化"));
+     closeButton->setToolTip(tr("关闭"));
+     //设置最小化、关闭按钮的样式
+     minButton->setStyleSheet("background-color:transparent;");
+     closeButton->setStyleSheet("background-color:transparent;");
+
+
 
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_tabWidget_currentChanged(int index)
+{
+    QString strDisplay = QString::number(index) + "is selected";
+
+    ui->label_ForDebug->setText(strDisplay);
+
+    switch(index)
+    {
+    case 0:
+    {
+
+    }
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    default:
+        break;
+
+
+    }
 }
